52031f22ff231f035cd8ad206b84457271113dd6
Ahmed <medosamsam919@gmail.com>
2026-05-20 20:34:12 +0300

    add link and make it as button  by adding buttonVaraints which takes the variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import { cva, type VariantProps } from "class-variance-authority";
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   "inline-flex items-center justify-center  font-medium transition-all duration-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 cursor-pointer",
   {
     variants: {
